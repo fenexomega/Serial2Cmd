f74f5b302375c9af6a006d924ea9b6947b5939d4
Jordy <jordyfgomes@gmail.com>
2017-02-11 14:06:11 -0300

    update

--- serial2cmd/config.py
+++ serial2cmd/config.py
@@ -1,6 +1,8 @@
 import os
 
-ICON_FILE   =   'icons/icon128.png'
-TRAY_ICON_FILE = 'icons/icon32.png'
-CONFIG_DIR 	=	os.getenv("HOME") + '/.config/serial2cmd/'
-CONFIG_FILE =	CONFIG_DIR + 'config.json'
+FILE_DIR        =   os.path.dirname(os.path.abspath(__file__))
+ICON_FILE       =   os.path.join(FILE_DIR,'icons','icon128.png')
+TRAY_ICON_FILE  =   os.path.join(FILE_DIR,'icons','icon32.png')
+CONFIG_DIR 	    =	os.path.join(os.getenv("HOME"),'.config','serial2cmd')
+CONFIG_FILE     =	os.path.join(CONFIG_DIR, 'config.json')
+DEFAULT_CONFIG_FILE = os.path.join(FILE_DIR,'data','config.json')
